58dc146230512066a889302797a15b6e080db14a
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-06-04 20:20:35 -0500

    translate finish

--- src/app/biographies/components/bio-card/bio-card.component.ts
+++ src/app/biographies/components/bio-card/bio-card.component.ts
@@ -1,18 +1,30 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Value } from '../../interfaces/bio.interface';
 import { MatDividerModule } from '@angular/material/divider';
-import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-bio-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatDividerModule, CommonModule],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule, TranslateModule],
   templateUrl: './bio-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./bio-card.component.css'],
 })
 export class BioCardComponent {
   bio = input.required<Value>();
+
+
+  translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
--- src/app/who-front/pages/home-page/home-page.component.ts
+++ src/app/who-front/pages/home-page/home-page.component.ts
@@ -2,13 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { BiographieService } from '../../../biographies/services/bio.service';
 import { BioCardComponent } from '../../../biographies/components/bio-card/bio-card.component';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [BioCardComponent, TranslateModule],
+  imports: [BioCardComponent,  TranslateModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.components.css'],
 })
@@ -16,10 +15,22 @@ export class HomePageComponent {
   limit = signal(10);
   bioService = inject(BiographieService);
 
+
   biographiesResource = rxResource({
     request: () => ({ top: this.limit() }),
     loader: ({ request }) => {
       return this.bioService.getBiographies({ $top: request.top });
     }
   });
+
+  translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
